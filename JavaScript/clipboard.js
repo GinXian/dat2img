3180bab4fbe55ded6377ea691c3552ce34e71d77
Gin Xian <1311134503@qq.com>
2021-07-13 18:33:24 +0800

    refactor: use `event.target` instead The previous implementation uses an wrapping function to create disposable closure to ensure the event listener can access its host element. However, event listener will receive a default parameter of `Event` type, which possesses a property `target` referencing the object to which the event listener is dispatched.

--- JavaScript/clipboard.js
+++ JavaScript/clipboard.js
@@ -4,24 +4,21 @@ var buttons = document.getElementsByClassName('copy');
 for(let button of buttons) {
     button.addEventListener(
         "click",
-        (function (button) {
-            var copyText = button.previousElementSibling.innerText;
-            return function listener() {
-                navigator.permissions.query({name: "clipboard-write"}).then(
-                    result => {
-                        if(result.state == 'granted' || result.state == 'prompt') {
-                            navigator.clipboard.writeText(copyText.trim()).then(
-                                // Success
-                                () => console.log("Copying to the clipboard succeeds."),
-                                // Error
-                                () => console.log("Fxxk!")
-                            )
-                        }
-                    }, 
-                    () => console.log('Permission denied')
-                )
-            }
-        })(button),
+        function listener(event) {
+            var copyText = event.target.previousElementSibling.innerText;
+            navigator.permissions.query({name:"clipboard-write"}).then(
+                result => {
+                    if(result.state == 'granted' || result.state == 'prompt') {
+                        navigator.clipboard.writeText(copyText).then(
+                            // Success
+                            () => console.log('Copying to the clipboard succeeds'),
+                            // Error
+                            () => console.log('Fxxk!')
+                        )
+                    }
+                }
+            )
+        },
         true, true
     );
 }
